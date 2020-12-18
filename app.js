812464644f73bed3bef7282c6a580b42c3acf0d0
yimialmonte <yelmialmonte@gmail.com>
2020-12-18 10:19:01 -0400

    created another methods for compare dino with human

--- app.js
+++ app.js
@@ -53,13 +53,44 @@ Dino.prototype.compareWeight = function(human) {
     this.fact = `${this.specie} has the same weight as human.`
   }
 }
+
 // Create Dino Compare Method 2
 // NOTE: Weight in JSON file is in lbs, height in inches.
-Dino.prototype.compareDeit = function(human) {
+Dino.prototype.compareHeight = function(human) {
+  const inchesInFeet = 12
+  const humanHeight = (human.height.feet * inchesInFeet) + human.feet.inches;
+  const dinoHeight = Number(this.height)
+
+  if(dinoHeight > humanHeight){
+    this.fact = `${this.specie} has ${dinoHeight - humanHeight} more height than human.`
+  } else if(dinoHeight < humanHeight) {
+    this.fact = `${this.specie} has ${humanHeight - dinoHeight} less height than human.`
+  } else {
+    this.fact = `${this.specie} has the same height as human.`
+  }
 }
 // Create Dino Compare Method 3
 // NOTE: Weight in JSON file is in lbs, height in inches.
+function eatBaseOnDiet(diet) {
+  switch(diet) {
+    case "herbavor":
+      return ["Plants", "Foliage", "Marine Algae"];
+    case "carnivor":
+      return ["Meat", "Animal tissue", "Insects"]
+    case "Omnivor":
+      return ["Plants", "Meat", "Combine Both"]
+    default:
+      return ["Nothing", "Nothing", "Nothing"];
+  }
+}
 
+Dino.prototype.compareDiet = function(human) {
+  if(this.diet === human.diet) {
+    this.fact = `This ${this.specie} has the same diet ${this.diet} as human.`
+  } else {
+    this.fact = `This ${this.specie} eat ${eatBaseOnDiet(this.diet)} and human eat: ${eatBaseOnDiet(human.diet)}`
+  }
+}
 // Generate Tiles for each Dino in Array
 
 // Add tiles to DOM
